04a567712210ee3ff3a6fa46575d1ad1bff594d1
Panagiotis <pskarl@gmail.com>
2017-01-23 20:25:29 +0200

    Assignemnt 3 progress

--- Module3Assignment3/app.js
+++ Module3Assignment3/app.js
@@ -1,4 +1,67 @@
 (function(){
-  angular.module('ShoppingListCheckOff', []);
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', foundItems)
+  .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com/menu_items.json');
 
+//CONTROLLER
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var list= this;
+  list.term = '';
+  list.found = [];
+//Search Item
+  list.search = function(searchTerm) {
+    if(searchTerm) {
+        var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+        promise.then(function(response){
+        return list.found = response;
+        })
+        .catch(function(error){
+        console.error(error);
+        });
+    } else {
+         return list.found = 0;
+      }
+  };
+// Delete Item 
+  list.deleteItem = function(index) {
+    list.found.splice(index, 1);
+  }
+
+};
+//SERVICE
+ MenuSearchService.$inject=['$http', 'ApiBasePath'];
+ function MenuSearchService($http, ApiBasePath) {
+   var service = this;
+   var arrItems =[];
+   service.getMatchedMenuItems = function(searchTerm){
+     var response = $http({
+       method: 'GET',
+       url: (ApiBasePath)
+     });
+  
+     return response.then(function(result) {
+      for(var i=0; i < result.data.menu_items.length; i++) {
+         if (result.data.menu_items[i].description.match(searchTerm)) {
+           arrItems.push(result.data.menu_items[i]);
+         }
+       }
+       console.info('Results: ' + arrItems.length + '\n Filtered Items: ' + arrItems);
+       return arrItems;
+     });
+   }
+ };
+
+
+
+//DIRECTIVE
+function foundItems() {
+ var ddo = {
+   templateUrl: 'found-items.html'
+ }
+ return ddo;
+};
+ 
 })();
